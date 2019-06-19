f687984b7992adb7df1fbeb753fb3b92dfe7bda1
Patrick <7809196+PatrickH123@users.noreply.github.com>
2019-06-19 18:21:50 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -113,7 +113,7 @@ function searchLostart(fName, lName, title, authority){
 			    	   var a = document.getElementsByTagName("a");
 			     	   Array.from (a).forEach (((x) => { x.setAttribute("target", "_blank"); }) );
 					});
-				}, (3000 * j));
+				}, (3000 * k));
             }
             }
 	});
