3b9f0b6b2ae497c05e1bb5aaf424bd488fdde9f0
Patrick <7809196+PatrickH123@users.noreply.github.com>
2019-06-19 17:29:35 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -93,7 +93,7 @@ function searchLostart(fName, lName, title, authority){
             Array.from (a).forEach (((x) => { x.setAttribute("target", "_blank"); }) );
             
             let arr = [0, 1, 2, 3, 4, 5, 6 ,7 ,8, 9];
-            if(arr.findIndex(x => x == data.split("<caption>")[1].charAt(0)) > -1){
+            if(arr.findIndex(x => x == data.split("<caption>")[1][0]) > -1){
             var results = data.split("<caption>")[1].split(" Search results")[0];
             document.getElementById("resultsAmount").innerHTML = document.getElementById("resultsAmount").innerHTML + "lostart.de returned " + results.toString() + " results<br>";
         	var pages = Math.ceil(parseInt(results)/10);
