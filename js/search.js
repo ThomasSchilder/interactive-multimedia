bd95f98e9535957697bae9a2f3a1b56d545eae88
Patrick <7809196+PatrickH123@users.noreply.github.com>
2019-06-19 12:18:25 +0200

    Recreate

---
+++ js/search.js
@@ -0,0 +1,41 @@
+function searchAll(){
+  var fName = document.getElementById("fName").value;
+  var lName = document.getElementById("lName").value;
+  var title = document.getElementById("title").value;
+  var authority = document.getElementById("authority").value;
+  var nationality = document.getElementById("nationality").value;
+  searchRkd(fName, lName);
+  searchLootedArt(fName, lName, title, authority, nationality);
+}
+
+function searchLootedArt(fName, lName, title, authority, nationality){
+	$.get("https://cors-anywhere.herokuapp.com/https://www.lootedart.com/search/searchdisplay.php\
+		?txtArtistLastName=" + fName + "\
+		&txtArtistFirstName=" + lName + "\
+		&cboNat=ANY&txtObjectStatus=ANY\
+		&txtPeriodDesc=ANY\
+		&txtObjectTitle=" + title + "\
+		&txtObjectType=ANY\
+		&cboPeriodStartYear=\
+		&cboPeriodStartEra=AD\
+		&cboPeriodEndYear=\
+		&cboPeriodEndEra=AD\
+		&txtOrientation=ANY\
+		&txtContactLastName=" + authority + "\
+		&txtContactFirstName=\
+		&txtLocationCity=\
+		&txtLocationCountry=" + nationality + "\
+		&txtStartYearComment=\
+		&txtEndYearComment=\
+		&submit1=Search", function(data) {
+		htmlresults = new DOMParser().parseFromString(data, "text/html");
+		var table = htmlresults.getElementsByTagName('table')[0].innerHTML;
+		$('#results').append(table);
+	});
+}
+
+function searchRkd(fName, lName){
+    $.get("/rkdcall?fName=" + fName + "&lName=" + lName, function(data) {
+        console.log(data);
+    });
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var mysql = require('mysql');
+var rkd = require('./parseToJson');
+
+http.createServer(function (req, res) {
+if(req.url == "/"){
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+} else if(req.url == "/js/search.js"){
+   fs.readFile('js/search.js', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/javascript'});
+    res.write(data);
+    res.end();
+  });
+} else if(req.url == "/js/main.js"){
+   fs.readFile('js/main.js', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/javascript'});
+    res.write(data);
+    res.end();
+  });
+} else if(req.url == "/js/parseToJson.js"){
+   fs.readFile('js/parseToJson.js', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/javascript'});
+    res.write(data);
+    res.end();
+  });
+} else if(req.url == "/css/stylesheet.css"){
+   fs.readFile('css/stylesheet.css', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/css'});
+    res.write(data);
+    res.end();
+  });
+} else if(req.url == "/images/logoartbase.jpeg"){
+   fs.readFile('images/logoartbase.jpeg', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+    res.write(data);
+    res.end();
+  });
+} else if(req.url.includes("/rkdcall")){
+    var q = url.parse(req.url.replace("rkdcall", ""), true).query;
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    rkd.rkdCall(q.fName, q.lName, function(data){
+      res.end(JSON.stringify(data));
+    });
+} else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.write("Not found");
+    res.end();
+}
+}).listen(8080);
