a52345344ab9ba932be9939f5fc841c380db2a99
ThomasSchilder <thomas.m.schilder@gmail.com>
2019-06-20 13:41:08 +0200

    jo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function switchToResults(){
+  document.getElementByClassName('search').style.display = "none";
+  document.getElementByClassName('results').style.display = "block";
+}
+
+function switchToSearch(){
+  document.getElementByClassName('serach').style.display = "block";
+  document.getElementByClassName('results').style.display = "none";
+}
