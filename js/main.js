600e7b085e506c21af1f52e241f4c4ea10cc9143
Patrick <7809196+PatrickH123@users.noreply.github.com>
2019-06-24 15:54:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,4 +6,13 @@ function switchToResults(){
 function switchToSearch(){
   document.getElementsByClassName('search')[0].style.display = "block";
   document.getElementsByClassName('results')[0].style.display = "none";
+  document.getElementById("results").innerHTML = '<table><caption id="resultsAmount"></caption></table><br>';
+  document.getElementById("lostart").innerHTML = "";
+  document.getElementById("lostart").setAttribute("alreadySearched", "false");
+  document.getElementById("lootart").innerHTML = "";
+  document.getElementById("lootart").setAttribute("alreadySearched", "false");
+  document.getElementById("herkomstgezocht").innerHTML = "";
+  document.getElementById("herkomstgezocht").setAttribute("alreadySearched", "false");
+  document.getElementById("rkd").innerHTML = "";
+  document.getElementById("rkd").setAttribute("alreadySearched", "false");
 }
