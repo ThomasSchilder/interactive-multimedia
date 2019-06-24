8194eaf6dbe830e43febf4667a951ca7e2f572e4
Patrick <7809196+PatrickH123@users.noreply.github.com>
2019-06-24 16:14:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ function switchToResults(){
 
 function switchToSearch(){
   document.getElementsByClassName('search')[0].style.display = "block";
-  document.getElementsByClassName('results')[0].style.display = "none";
+  document.getElementById('results')[0].style.display = "none";
   document.getElementById("results").innerHTML = '<table><caption id="resultsAmount"></caption></table><br>';
-  document.getElementByClassName("lostart")[0].innerHTML = "";
-  document.getElementByClassName("lostart")[0].setAttribute("alreadySearched", "false");
-  document.getElementByClassName("lootart")[0].innerHTML = "";
-  document.getElementByClassName("lootart")[0].setAttribute("alreadySearched", "false");
-  document.getElementByClassName("herkomstgezocht")[0].innerHTML = "";
-  document.getElementByClassName("herkomstgezocht")[0].setAttribute("alreadySearched", "false");
-  document.getElementByClassName("rkd")[0].innerHTML = "";
-  document.getElementByClassName("rkd")[0].setAttribute("alreadySearched", "false");
+  document.getElementsByClassName("lostart")[0].innerHTML = "";
+  document.getElementsByClassName("lostart")[0].setAttribute("alreadySearched", "false");
+  document.getElementsByClassName("lootart")[0].innerHTML = "";
+  document.getElementsByClassName("lootart")[0].setAttribute("alreadySearched", "false");
+  document.getElementsByClassName("herkomstgezocht")[0].innerHTML = "";
+  document.getElementsByClassName("herkomstgezocht")[0].setAttribute("alreadySearched", "false");
+  document.getElementsByClassName("rkd")[0].innerHTML = "";
+  document.getElementsByClassName("rkd")[0].setAttribute("alreadySearched", "false");
 }
